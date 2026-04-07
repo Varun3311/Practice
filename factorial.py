8d497474263797ae5fa88fac109c0af0d9f0a6a7
Varun3311 <karravarunreddy@gmail.com>
2026-04-07 10:45:52 -0400

    added recursion factorial

--- factorial.py
+++ factorial.py
@@ -9,9 +9,21 @@ def fact(n):
             f= f*i
         print(f)
 
-num=int(input("enter number:"))
-fact(num)
+numb=int(input("enter number:"))
+fact(numb)
 
 
 
 #Using recursion:
+
+def rec_fact(num):
+    if num==0 or num==1:
+        return 1
+    elif num<0:
+        return "cannot define"
+    else:
+        return num*rec_fact(num-1)
+
+num=int(input("enter a num:"))
+result=rec_fact(num)
+print(result)
