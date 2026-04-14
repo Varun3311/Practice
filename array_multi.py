a7b1659937dbb09db8d9592f1c1bafd400baabfe
Varun3311 <karravarunreddy@gmail.com>
2026-04-14 09:51:31 -0400

    converting to array

--- array_multi.py
+++ array_multi.py
@@ -1,10 +1,11 @@
 '''Python Program for Find Remainder of Array squaring and adding Divided by N'''
-
+import numpy as np
 nums=int(input("enter number of elements in array:"))
-array=[]
+array1=[] #list 
 for i in range(nums):
     n=int(input("enter number:"))
-    array.append(n)
+    array1.append(n)
+array=np.array(array1) #converting to array type
 N=int(input("enter N:"))
 sum=0
 for i in array:
@@ -12,6 +13,6 @@ for i in array:
 
 print(sum%N)
 
-
+print(type(array))
 
 
